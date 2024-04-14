9d3b74bb6073c38074925e11f5283380e5674e41
Thalia Papageorgiou <tpapage@emory.edu>
2024-04-14 17:18:21 -0400

    helpers

---
+++ Alphas/neural_net.py
@@ -0,0 +1,17 @@
+from sklearn.neural_network import MLPClassifier
+import pandas as pd
+import numpy as np
+
+def neural_net(x, y):
+    # x = x.to_frame()
+    # y = y.to_frame()
+    clf = MLPClassifier(random_state=1, max_iter=300).fit(x, y)  #TODO: change random state after testing is over possibly? 
+    return clf
+
+def main():
+    x = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)))
+    y = pd.DataFrame(np.random.randint(0, 2, size=(100, 1)))
+    print(neural_net(x, y))
+
+if __name__ == "__main__":
+    main()
