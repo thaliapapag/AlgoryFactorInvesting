81c2c98f10abcf33e94e29e0867cb0aa74663fdd
pvloiselle <124627176+pvloiselle@users.noreply.github.com>
2024-04-19 15:03:30 -0400

    Added alphas 12-15

--- Alphas/Alpha1-25.py
+++ Alphas/Alpha1-25.py
@@ -5,6 +5,9 @@ import json
 import os
 import time
 from tqdm import tqdm
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
 
 def alpha1(data):
     """
@@ -102,12 +105,80 @@ def alpha9(data):
     condition2 = ts_max < 0
     return np.where(condition1, delta_close, np.where(condition2, delta_close, -delta_close))
 
+# Alphas
 def alpha10(data):
-    delta_close = data['close'].diff()
+    delta_close = data['Close'].diff()
     ts_min = delta_close.rolling(window=4).min()
     ts_max = delta_close.rolling(window=4).max()
     condition1 = ts_min > 0
     condition2 = ts_max < 0
     alpha = np.where(condition1, delta_close, np.where(condition2, delta_close, -delta_close))
-    return alpha.rank()
+    return np.array(pd.Series(alpha).rank())
 
+
+#### I am making the ones below here work. ####
+
+# (sign(delta(volume, 1)) * (-1 * delta(close, 1)))
+
+def alpha12(data): # Works - will have to drop the first day
+    delta_volume = data['Volume'].diff()
+    delta_close = data['Close'].diff()
+    return np.sign(delta_volume) * (-1 * delta_close)
+
+def alpha13(data): # drops the fist 4 values
+    # (-1 * rank(covariance(rank(close), rank(volume), 5)))
+
+    rank_close = data['Close'].rank()
+    rank_volume = data['Volume'].rank()
+
+    covariance = rank_close.rolling(window=5).cov(rank_volume)
+    rank_covariance = covariance.rank()
+
+    return -1 * rank_covariance
+
+
+
+
+def alpha14(data):  # Will have to drop the first 9 days from the dataset each time because they are all nan due to the
+                    # correlation being over the previous 10 days. This is expected, and we can offset this by including
+                    # 9 extra days at the beginning of the dataset, specifically for this alpha
+
+    # ((-1 * rank(delta(returns, 3))) * correlation(open, volume, 10))
+
+    delta_returns = ((data['Close'] - data['Open']) / data['Open']).diff(3)
+    open_volume_correlation = data['Open'].rolling(window=10).corr(data['Volume'])
+    return -1 * delta_returns.rank() * open_volume_correlation
+
+def alpha15(data): # Works - will have to drop the first 6 days each time
+
+    # (-1 * sum(rank(correlation(rank(high), rank(volume), 3)), 3))
+
+    high_rank = data['High'].rank()
+    volume_rank = data['Volume'].rank()
+    correlation = high_rank.rolling(window=3).corr(volume_rank)
+    rank_correlation = correlation.rolling(window=3).apply(lambda x: pd.Series(x).rank().iloc[-1])
+    return -1 * rank_correlation.rolling(window=3).sum()
+
+
+
+
+
+
+
+
+
+
+def main():
+    
+    end_date = datetime.now() - timedelta(days=50)
+    start_date = end_date - timedelta(days=15)
+
+    ticker = 'GOOG'
+
+    data = yf.download(ticker, start=start_date.strftime('%Y-%m-%d'), end=end_date.strftime('%Y-%m-%d'))
+    # print(data)
+
+    print(alpha13(data))
+
+if __name__ == "__main__":
+    main()
